372e1a76d659377e278ea8d9164ac8adb6e24880
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2018-01-09 19:29:19 -0800

    started filling out the work section.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,7 @@ $(window).scroll(function() {
 
 // $('.other-links a').first().delay(100).animate({'opacity':'1'},600);
 // $('.other-links a').last().delay(400).animate({'opacity':'1'},600);
-$('.heroFirst').delay(600).animate({'opacity':'1'},1500);
+$('.heroText').delay(800).animate({'opacity':'1'},1500);
+$('.heroFirst').delay(800).animate({'opacity':'1'},1500);
 $('.heroSecond').delay(1000).animate({'opacity':'1'},1500);
 $('.heroThird').delay(1300).animate({'marginTop':'+=-80px','opacity':'1'},800);
